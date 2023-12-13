cc216612eef820075a99b906bb12136d12cd1ac2
BradFeng02 <bradfeng02@gmail.com>
2023-12-13 14:34:31 -0500

    dotenv with spoonacular secret

--- web_app/app.py
+++ web_app/app.py
@@ -3,14 +3,25 @@ flask app for team 5's final project
 """
 
 from flask import Flask, render_template
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+
+
+def fetch_spoon_api():
+    """fetch from Spoonacular API"""
+    pass   
 
 
 @app.route("/")
 def index():
     """Renders home page"""
     return render_template("index.html")
+
+@app.route("/recipe/<recipe_id>")
+def recipe(recipe_id=639413):
+    pass
 
 
 if __name__ == "__main__":
