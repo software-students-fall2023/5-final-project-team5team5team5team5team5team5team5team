87d5163ca2c7ff3ae01585d6075e82bf6e754428
BradFeng02 <bradfeng02@gmail.com>
2023-12-13 23:50:51 -0500

    remove test data, lint, format

--- web_app/app.py
+++ web_app/app.py
@@ -3,6 +3,7 @@ flask app for team 5's final project
 """
 
 from os import getenv
+from sys import exit as sysexit
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import requests
@@ -14,9 +15,10 @@ app = Flask(__name__)
 SPOON_SECRET = getenv("SPOON_SECRET")
 if not SPOON_SECRET:
     print("Spoonacular secret not found.")
-    exit()
+    sysexit()
 
-def fetch_spoon_api(path: str, query: dict[str, str]=None):
+
+def fetch_spoon_api(path: str, query: dict[str, str] = None):
     """
     Fetch from Spoonacular API.
     GET path with query string.
@@ -24,13 +26,13 @@ def fetch_spoon_api(path: str, query: dict[str, str]=None):
     """
     if query is None:
         query = {}
-    r = requests.get(path, params={**query, "apiKey": SPOON_SECRET})
+    r = requests.get(path, params={**query, "apiKey": SPOON_SECRET}, timeout=10)
     print(f"Spoonacular API - GET {r.url}")
     if r.status_code != 200:
         print(f"Spoonacular API - error:\n{r}")
         return None
-    else:
-        return r.json()
+
+    return r.json()
 
 
 @app.route("/")
@@ -38,16 +40,19 @@ def index():
     """Renders home page"""
     return render_template("index.html")
 
+
 # target nutrients
 NUTRIENTS = {"calories", "fat", "sodium", "carbohydrates", "sugar", "protein", "fiber"}
+
+
 @app.route("/recipe/<recipe_id>")
 def recipe(recipe_id=639413):
-    from recipe_test_res import RECIPE_TEST_RES
-    # res = fetch_spoon_api(
-    #     f"https://api.spoonacular.com/recipes/{recipe_id}/information",
-    #     { "includeNutrition": True }
-    # )
-    res = RECIPE_TEST_RES
+    """Renders recipe details page"""
+
+    res = fetch_spoon_api(
+        f"https://api.spoonacular.com/recipes/{recipe_id}/information",
+        {"includeNutrition": True},
+    )
 
     # nutrients
     nutrition = {
